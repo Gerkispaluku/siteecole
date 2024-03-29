c1a487291942f08b4900c7e508d5a9842aa1d38c
Gerkis <gerckis90@gmail.com>
2024-03-29 16:23:41 +0200

    premier comit du projet ecole avec un menu qui marche deja et les defilements des images

---
+++ script.js
@@ -0,0 +1,13 @@
+new Swiper('.swiper-slider',
+{
+    centredSlider: true,
+    slidesPerView:3,
+    spaceBetween: 10,
+    mousewheel:false,
+    autoplay:{
+        delay: 2000,
+        desableOninteraction: false,
+    }
+
+}
+);
